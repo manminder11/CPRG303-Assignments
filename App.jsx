c9b27ff1ae87869b4d50f7dace1c78371c654f8c
Manminder <ManminderSingh@edu.sait.ca>
2024-10-30 11:22:48 -0600

    for now it is okk

--- App.jsx
+++ App.jsx
@@ -4,52 +4,38 @@
  * @format
  */
 
-import React from 'react';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-import { useState } from 'react';
-import {
-  SafeAreaView,
-  Pressable,
-  View,
-  ScrollView,
-} from 'react-native';
-
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { SafeAreaView, Pressable, View, ScrollView, Text } from "react-native";
 
 function App() {
-  const [todos, setTodos] = useState([
-    'Do the laundry',
-    'Take out the trash',
-    'go to the gym',
-    'walk the dog',
-  ]);
+  const [todos, setTodos] = useState(["Do laundry", "Go to gym", "Walk dog"]);
 
   const addTodo = (todo) => {
     setTodos([...todos, todo]);
   };
-
-  map = (todos) => {
-    return todos.map((todo, index) => {
-      return (
-        <View key={index}>
-          <Text>{todo}</Text>
-        </View>
-      );
-    });
-  }
 
   return (
     <SafeAreaView>
       <ScrollView>
         <Pressable>
           <View>
-            <TodoList todos={todos} />
+            <TodoList tasks={todos} />
             <TodoForm addTodo={addTodo} />
           </View>
         </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
+}
+
+function List({ tasks }) {
+  return tasks.map((task, index) => (
+    <View key={index}>
+      <Text>{task}</Text>
+    </View>
+  ));
 }
 
 export default App;
