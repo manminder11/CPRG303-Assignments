bc1a967245b91e524fdf4489a3278cb490dad1b0
Manminder <ManminderSingh@edu.sait.ca>
2024-10-30 11:05:12 -0600

    maping the todos

--- App.jsx
+++ App.jsx
@@ -7,6 +7,7 @@
 import React from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import { useState } from 'react';
 import {
   SafeAreaView,
   Pressable,
@@ -16,13 +17,34 @@ import {
 
 
 function App() {
+  const [todos, setTodos] = useState([
+    'Do the laundry',
+    'Take out the trash',
+    'go to the gym',
+    'walk the dog',
+  ]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  map = (todos) => {
+    return todos.map((todo, index) => {
+      return (
+        <View key={index}>
+          <Text>{todo}</Text>
+        </View>
+      );
+    });
+  }
+
   return (
     <SafeAreaView>
       <ScrollView>
         <Pressable>
           <View>
-            <TodoList />
-            <TodoForm />
+            <TodoList todos={todos} />
+            <TodoForm addTodo={addTodo} />
           </View>
         </Pressable>
       </ScrollView>
